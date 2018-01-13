3c8c46b8b7e8d02257ca1d8ba59176edeaab7c4d
Kyle VanDeRiet <kylevdr@gmail.com>
2018-01-13 12:49:57 -0700

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Kyle VanDeRiet on 1/13/18.
+//  Copyright © 2018 Kyle VanDeRiet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
